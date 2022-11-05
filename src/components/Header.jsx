2eb667b745d41212fa6a3b9fff6613b7582ce459
Mika2901 <stronghit2901@gmail.com>
2022-11-05 15:07:03 +0400

    app init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://t3.ftcdn.net/jpg/02/84/87/88/360_F_284878821_E2nyf8wkMPLWnnn0D7DdgYAv7ngrCQTH.jpg'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://img2.badfon.ru/wallpaper/big/5/cc/tigr-hischnik-vzglyad-vodopad-6735.jpg'></img>
+        </div>
+        <div>
+            <img src='https://papik.pro/uploads/posts/2021-11/1637289678_1-papik-pro-p-smeshnoi-tigr-risunok-1.jpg'></img>
+        </div>
+        <div>
+            my post
+        </div>
+        <div>
+            new post
+        </div>
+        <div>
+            <div>
+                post1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
